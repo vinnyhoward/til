8ee726caca6f8d6ef328fb89f6789527612ec9c4
Vince Howard <vincenguyenhoward@gmail.com>
2019-02-26 20:26:39 -0700

    learned about advanced binary search and learned about sequenceSum

---
+++ javascript/toy-problems/sum_of_sequence.js
@@ -0,0 +1,28 @@
+// Sum of a sequence
+
+// Your task is to make function, which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step.
+
+// If begin value is greater than the end, function should returns 0
+
+// Examples
+
+// sequenceSum(2,2,2) === 2
+// sequenceSum(2,6,2) === 12 // 2 + 4 + 6
+// sequenceSum(1,5,1) === 15 // 1 + 2 + 3 + 4 + 5
+// sequenceSum(1,5,3) === 5 // 1 + 4
+
+const sequenceSum = (begin, end, step) => {
+	if (begin > end) {
+		return 0;
+	}
+	return begin + sequenceSum(begin + step, end, step);
+};
+
+// console.log(sequenceSum(2, 6, 2));
+// console.log(sequenceSum(1, 5, 1));
+// console.log(sequenceSum(1, 5, 3));
+console.log(sequenceSum(520, 448, 57));
+console.log(sequenceSum(0, 15, 3));
+// 45
