fd4a11a1e2c8c8167d4e71ccad91f3e2ed2e8400
Vince Howard <vincenguyenhoward@gmail.com>
2019-05-30 23:58:31 -0600

    learne more about pointers and solved a js problem using it

--- javascript/toy-problems/sum_zero.js
+++ javascript/toy-problems/sum_zero.js
@@ -1,19 +1,38 @@
 
 
-const sumZero = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === 0) {
-        return [arr[i], arr[j]]
-      }
-    }
-  }
-}
+// const sumZero = (arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === 0) {
+//         return [arr[i], arr[j]]
+//       }
+//     }
+//   }
+// }
 
-sumZero([3, -2, -1, 0, 1, 2, 3]) // [3. -3]
-sumZero([-2, 0, 1, 3]) // undefined
-sumZero([1, 2, 3]) // undefined
+// sumZero([3, -2, -1, 0, 1, 2, 3]) // [3. -3]
+// sumZero([-2, 0, 1, 3]) // undefined
+// sumZero([1, 2, 3]) // undefined
 
-console.log(sumZero([3, -2, -1, 0, 1, 2, 3]))
-console.log(sumZero([-2, 0, 1, 3]))
-console.log(sumZero([1, 2, 3]))
+// console.log(sumZero([3, -2, -1, 0, 1, 2, 3]))
+// console.log(sumZero([-2, 0, 1, 3]))
+// console.log(sumZero([1, 2, 3]))
+
+const sumZeroAlt = (arr) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while(left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+      left ++
+    };
+  };
+};
+
+console.log(sumZeroAlt([3, -2, -1, 0, 1, 2, 3]))
+console.log(sumZeroAlt([-2, 0, 1, 3]))
+console.log(sumZeroAlt([1, 2, 3]))
