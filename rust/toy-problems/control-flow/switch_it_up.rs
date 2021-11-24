acb5f09f558dd473ba33d7b7e328691425239222
Vince Howard <vincenguyenhoward@gmail.com>
2021-11-23 22:00:16 -0700

    solved a super easy toy problem

---
+++ rust/toy-problems/control-flow/switch_it_up.rs
@@ -0,0 +1,22 @@
+// Switch Up
+
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+fn switch_it_up(n: usize) -> &'static str {
+    match n {
+        1 => "One",
+        2 => "Two",
+        3 => "Three",
+        4 => "Four",
+        5 => "Five",
+        6 => "Six",
+        7 => "Seven",
+        8 => "Eight",
+        9 => "Nine",
+        _ => "Zero"
+    }
+}
