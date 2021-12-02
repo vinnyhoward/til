2b122f70beee4e2060a39da492b330556ce2403d
Vince Howard <vincenguyenhoward@gmail.com>
2021-12-02 11:19:49 -0700

    solved a simple rust problem and learned about string interpolation using the maco format! in rust

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ A collection of small things that I've learned and toy problems that I have done
 
 _Streaks:_
 
-_29 TILs in a row so far..._
+_30 TILs in a row so far..._
 
-_29 Toy problems completed in a row and counting..._
+_30 Toy problems completed in a row and counting..._
 
 _Previous streak: 23 TILs😢 and 0 toy problems..._
 
---
+++ rust/toy-problems/strings/returning_strings.rs
@@ -0,0 +1,14 @@
+// Returning Strings
+
+// Make a function that will return a greeting statement that uses an input; your program should return, 
+// "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+fn main() {
+    fn greet(name: &str) -> String {
+        let greeting = format!("Hello, {} how are you doing today?", name);
+        return greeting;
+    }
+    println!("{}", greet("Courtney"));
+}
